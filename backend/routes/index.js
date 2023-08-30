3190faa9f9e49b7f6a786edd6211cbe663d1eeb1
Mirosława Borkowska <130639536+Mirabordem@users.noreply.github.com>
2023-08-30 19:02:22 -0400

    new

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -16,6 +16,13 @@ router.get("/api/csrf/restore", (req, res) => {
     });
   });
 
+  // backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
 
 
 
