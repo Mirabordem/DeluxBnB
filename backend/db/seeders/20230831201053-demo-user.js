1e7fb75aebfb62da585f39f37db5e99476bd3b77
Mirosława Borkowska <130639536+Mirabordem@users.noreply.github.com>
2023-09-01 15:45:32 -0400

    new

--- backend/db/seeders/20230831201053-demo-user.js
+++ backend/db/seeders/20230831201053-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Frank',
+        lastName: 'Wright',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Tom',
+        lastName: 'Long',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Anna',
+        lastName: 'Doe',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
