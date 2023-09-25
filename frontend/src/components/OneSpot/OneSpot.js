f065ea20da1c8c78a99aa0a0c1efdc7933832e1d
Mirosława Borkowska <130639536+Mirabordem@users.noreply.github.com>
2023-09-25 16:21:11 -0400

    spots

--- frontend/src/components/OneSpot/OneSpot.js
+++ frontend/src/components/OneSpot/OneSpot.js
@@ -6,18 +6,65 @@ import Tooltip from "../Tooltip/Tooltip";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
 
+function OneSpot({ spot }) {
+  let manage = false;
+  if (window.location.href.endsWith("current")) {
+    manage = true;
+  }
 
-
-function OneSpot({spot}) {
-    let manage = false;
-    if (window.location.href.endsWith('current')) {
-        manage = true;
-    }
-
-
-
-
+  return (
+    <div className="spot-container">
+      <div className="spot-image-container">
+        <Tooltip content={spot.name} direction="top">
+          <NavLink>
+            <img
+              className="one-spot-image"
+              src={spot.previewImage}
+              content={spot.name}
+              alt=""
+            />
+          </NavLink>
+        </Tooltip>
+      </div>
+      <NavLink
+        to={"/spots/${spot.id"}
+        style={{ textDecoration: "none", color: "black" }}
+      >
+        <div className="one-spot-info">
+          <div>
+            <h4 className="one-spot-location">
+              {spot.city}, {spot.state}
+            </h4>
+            <p className="one-spot-price">${spot.price} night</p>
+          </div>
+          <div className="star-rating">
+            {spot.avgRating >= 1 ? (
+              <div className="star-rating-container">
+                <i className="fa-solid fa-star fa-reviewstar"></i>
+                <p>{spot.avgRating.toFixed(2)}</p>
+              </div>
+            ) : (
+              <div className="star-rating-container">
+                <i className="fa-regular fa-star fa-reviewstar"></i>
+                <p className="one-spot-new">New</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </NavLink>
+      {manage && (
+        <div className="update-delete">
+          <NavLink to={`/spots/${spot.id}/edit`}>
+            <button className="managing-update">Update</button>
+          </NavLink>
+          <OpenModalMenuItem
+            modalComponent={<DeleteModal spot={spot} />}
+            itemText="Delete"
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
-
-export default SingleSpot;
+export default OneSpot;
