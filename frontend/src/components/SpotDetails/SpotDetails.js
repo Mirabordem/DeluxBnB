1b78f2384addcca8f599d563ae81f8f2c0938ec6
Mirosława Borkowska <130639536+Mirabordem@users.noreply.github.com>
2023-09-25 21:55:44 -0400

    images

--- frontend/src/components/SpotDetails/SpotDetails.js
+++ frontend/src/components/SpotDetails/SpotDetails.js
@@ -7,7 +7,6 @@ import "./SpotDetails.css";
 function SpotDetails() {
   const { spotId } = useParams();
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.session.user);
   const spot = useSelector((state) => state.spots.oneSpot);
 
   useEffect(() => {
@@ -17,15 +16,23 @@ function SpotDetails() {
   if (!spot || Object.keys(spot).length === 0) return null;
 
   const previewImage = spot.SpotImages.find((image) => image.preview);
+  console.log(
+    "🚀 ~ file: SpotDetails.js:19 ~ SpotDetails ~ previewImage:",
+    previewImage
+  );
   let otherImages = spot.SpotImages.filter((image) => !image.preview);
+  console.log(
+    "🚀 ~ file: SpotDetails.js:20 ~ SpotDetails ~ otherImages:",
+    otherImages
+  );
 
   if (otherImages.length < 4) {
     let index = otherImages.length;
-    const noImage = {
+    const emptyImage = {
       url: "https://image.jimcdn.com/app/cms/image/transf/none/path/sd0536822daf447dd/image/i3ef5a3008602d20d/version/1695685999/image.png",
     };
     while (index < 4) {
-      otherImages[index] = noImage;
+      otherImages[index] = emptyImage;
       index++;
     }
   }
@@ -40,87 +47,87 @@ function SpotDetails() {
     }
   };
 
-
   return (
-    <div className='wrapper'>
-        <div className='details-container'>
-            <h1 className='h1'>{spot.name}</h1>
-            <h2 className='h2'>{spot.city}, {spot.state}, {spot.country}</h2>
-            <div className='image-bundle'>
-                <img
-                className='preview-image'
-                src={Object.keys(previewImage).length > 0 && previewImage.url}
-                alt=''
-                ></img>
-                <div className='small-images-container'>
-                    {otherImages.map((image, index) => (
-                        <img
-                        className='small-images'
-                        id={
-                            (index === 1 && 'topright') || (index === 3 && 'bottomright')
-                        }
-                        scr={image.url}
-                        alt=''
-                        ></img>
-                    ))}
-                </div>
-            </div>
-            <div className='host'>
-                Hosted by:
-                <p className='host-name'>
-                    &nbsp;{spot.Owner.firstName} {spot.Owner.lastName}
-                </p>
-            </div>
-            <div className='info-container'>
-                <div className='description'>
-                    <p>{spot.description}</p>
-                    <p className='show-more'>Show more</p>
-                </div>
-                <div className='reserve-container'>
-                    <div className='reserve-top'>
-                        <p className='price'>
-                            <strong>${spot.price}</strong> night
-                        </p>
-                        <div className='star'>
-                            <i
-                            className={
-                                spot.numReviews > 0
-                                ? 'fa-solid fa-star fa-reviewstar'
-                                : 'fa-regular fa-star fa-reviewstar'
-                            }
-                            ></i>
-                            {spot.avgStarRating ? (
-                                <p className='number-stars'>
-                                    &nbsp;{spot.avgStarRating}
-                                </p>
-                            ) : null}
-                            {spot.numReviews > 0 ? <p className='dot'>・</p> : null}
-                            <p className={
-                                spot.numReviews > 0
-                                ? 'spotdetails-reviewtext'
-                                : 'spotdetails-newtext'
-                            }
-                            >
-                                {reviewsStatus(spot.numReviews)}
-                            </p>
-                        </div>
-                    </div>
-                    <button
-                    className='reserve-button'
-                    onClick={() => {
-                        alert('Feature coming soon!')
-                    }}
-                    >
-                        Reserve
-                    </button>
-                </div>
-            </div>
-            {/* <div>
-                <ReviewComponent spot={spot} user={user} reviews={objReviews} />
-            </div> */}
+    <div className="wrapper">
+      <div className="details-container">
+        <h1 className="h1">{spot.name}</h1>
+        <h2 className="h2">
+          {spot.city}, {spot.state}, {spot.country}
+        </h2>
+        <div className="image-bundle">
+          <img
+            className="preview-image"
+            src={Object.keys(previewImage).length > 0 && previewImage.url}
+            alt=""
+          ></img>
+          <div className="small-images-container">
+            {otherImages.map((image, index) => (
+              <img
+                className="small-images"
+                id={
+                  (index === 1 && "topright") ||
+                  (index === 3 && "bottomright") ||
+                  ""
+                }
+                src={image.url}
+                alt=""
+                key={image.id}
+              ></img>
+            ))}
+          </div>
         </div>
+        <div className="host">
+          Hosted by:
+          <p className="host-name">
+            &nbsp;{spot.Owner.firstName} {spot.Owner.lastName}
+          </p>
+        </div>
+        <div className="info-container">
+          <div className="description">
+            <p>{spot.description}</p>
+            <p className="show-more">Show more</p>
+          </div>
+          <div className="reserve-container">
+            <div className="reserve-top">
+              <p className="price">
+                <strong>${spot.price}</strong> night
+              </p>
+              <div className="star">
+                <i
+                  className={
+                    spot.numReviews > 0
+                      ? "fa-solid fa-star fa-reviewstar"
+                      : "fa-regular fa-star fa-reviewstar"
+                  }
+                ></i>
+                {spot.avgStarRating ? (
+                  <p className="number-stars">&nbsp;{spot.avgStarRating}</p>
+                ) : null}
+                {spot.numReviews > 0 ? <p className="dot">・</p> : null}
+                <p
+                  className={
+                    spot.numReviews > 0
+                      ? "spotdetails-reviewtext"
+                      : "spotdetails-newtext"
+                  }
+                >
+                  {reviewsStatus(spot.numReviews)}
+                </p>
+              </div>
+            </div>
+            <button
+              className="reserve-button"
+              onClick={() => {
+                alert("Feature coming soon!");
+              }}
+            >
+              Reserve
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-};
+  );
+}
 
 export default SpotDetails;
