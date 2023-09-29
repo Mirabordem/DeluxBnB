989de03d3492dd7ed48e8d6e87f6a07475061f1c
Mirosława Borkowska <130639536+Mirabordem@users.noreply.github.com>
2023-09-29 18:32:20 -0400

    fully functioning

--- frontend/src/components/Reviews/Reviews.js
+++ frontend/src/components/Reviews/Reviews.js
@@ -99,9 +99,10 @@ const Reviews = ({ spot, reviews, user }) => {
           }
         ></i>
 
-        {spot.avgStarRating > 0 ? (
-          <p className="stars-two">{parseInt(spot.avgRating).toFixed(2)}</p>
-        ) : null}
+
+        {spot.avgStarRating ? (
+                  <p className="number-stars">&nbsp;{spot.avgStarRating}</p>
+                ) : null}
         {spot.numReviews > 0 ? <p className="reviews-dot">·</p> : null}
         <p className={spot.numReviews > 0 ? "review-text" : "text-new"}>
           {checkReviews(spot.numReviews)}
@@ -113,7 +114,7 @@ const Reviews = ({ spot, reviews, user }) => {
         <p className="first-reviewer">Be the first to post a review!</p>
       ) : null}
 
-      {reviewBtn && (
+      {reviewBtn && sessionUser.id !== spot.Owner.id && (
         <button className="post-review">
           <OpenModalMenuItem
             itemText="Post a Review"
@@ -158,106 +159,3 @@ const Reviews = ({ spot, reviews, user }) => {
 };
 
 export default Reviews;
-
-
-
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchSpotReviews } from "../../store/reviews";
-// import { thunkGetDetails } from "../../store/spots";
-// import "./Reviews.css";
-// import CreateReviewModal from "./CreateReviewModal";
-// import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
-// import DeleteReviewModal from "./DeleteReviewModal";
-
-// const Reviews = ({ spot }) => {
-//   const dispatch = useDispatch();
-//   const sessionUser = useSelector((state) => state.session.user);
-//   const [reviewBtn, setReviewBtn] = useState(true);
-
-//   useEffect(() => {
-//     // Logic to determine if review button should be disabled
-//     if (sessionUser) {
-//       if (spot.OwnerId === sessionUser.id) {
-//         setReviewBtn(false);
-//       } else if (
-//         spot.Reviews.some((review) => review.userId === sessionUser.id)
-//       ) {
-//         setReviewBtn(false);
-//       }
-//     } else {
-//       setReviewBtn(false);
-//     }
-//   }, [spot, sessionUser]);
-
-//   useEffect(() => {
-//     dispatch(thunkGetDetails(spot.id));
-//     dispatch(fetchSpotReviews(spot.id));
-//   }, [dispatch, spot.id]);
-
-//   return (
-//     <div>
-//       <div className="reviews-container">
-//         <i
-//           className={`${
-//             spot.numReviews > 0
-//               ? "fa-solid fa-star fa-reviewstar"
-//               : "fa-regular fa-star fa-reviewstar"
-//           }`}
-//         ></i>
-//         {spot.avgStarRating > 0 ? (
-//           <p className="stars-two">{spot.avgStarRating.toFixed(2)}</p>
-//         ) : null}
-//         {spot.numReviews > 0 && <p className="reviews-dot">·</p>}
-//         <p className={spot.numReviews > 0 ? "review-text" : "text-new"}>
-//           {spot.numReviews === 0
-//             ? "Be the first to post a review!"
-//             : `${spot.numReviews} ${
-//                 spot.numReviews === 1 ? "Review" : "Reviews"
-//               }`}
-//         </p>
-//       </div>
-
-//       {reviewBtn && (
-//         <button className="post-review">
-//           <OpenModalMenuItem
-//             itemText="Post a Review"
-//             modalComponent={<CreateReviewModal spot={spot} sessionUser={sessionUser} />}
-//           />
-//         </button>
-//       )}
-
-//       <div className="reviews-container"></div>
-
-//       {spot.Reviews.map((review) => (
-//         <div className="single-review-container" key={review.id}>
-//           <div className="review-header">
-//             <img
-//               className="profile-icon"
-//               src={
-//                 "https://image.jimcdn.com/app/cms/image/transf/none/path/sd0536822daf447dd/image/i9a305a7efa48dc70/version/1695953827/image.png"
-//               }
-//               alt=""
-//             />
-//             <div className="name-date">
-//               <h3>{review.User.firstName}</h3>
-//               <p>{new Date(review.updatedAt).toLocaleDateString()}</p>
-//             </div>
-//           </div>
-//           <div className="single-description">{review.review}</div>
-
-//           {review.userId === sessionUser?.id && (
-//             <button className="review-delete-button">
-//               <OpenModalMenuItem
-//                 itemText="Delete"
-//                 modalComponent={<DeleteReviewModal spot={spot} review={review} />}
-//               />
-//             </button>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Reviews;
